64014858bbf0df82eb548b289f650b8f1e5972be
Alexander Graul <agraul@suse.com>
2022-05-20 16:03:04 +0200

    Add obs.list_subprojects()
    Needed to figure out all of the "Client Tools" subprojects dynamically.

--- src/bundle_dep_notifier/obs.py
+++ src/bundle_dep_notifier/obs.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import urllib.parse
 from typing import List
 from xml.etree import ElementTree
 
@@ -20,6 +21,7 @@ class Credentials:
 
     def as_tuple(self):
         return (self.username, self.password)
+
 
 def list_packages(
     project_name: str,
@@ -67,6 +69,39 @@ def package_was_updated(
     return _any_timestamp_is_newer(timestamps, last_check)
 
 
+def list_subprojects(
+    project_name: str,
+    credentials: Credentials,
+    api_url: str = "https://api.opensuse.org",
+) -> List[str]:
+    """List all OBS packages in an OBS project.
+
+    :param project_name: Name of OBS project
+    :param credentials: OBS API credentials
+    :param api_url: Base URL of the OBS API server, defaults to https://api.opensuse.org
+    :return: List of package names
+    """
+    response_text = _query_subprojects_list(
+        project_name=project_name,
+        credentials=credentials,
+        api_url=api_url,
+    )
+
+    return _parse_subprojects_list(response_text)
+
+
+def _query_subprojects_list(project_name: str, credentials: Credentials, api_url: str):
+    try:
+        url = f"{api_url}/search/project/id?match=" + urllib.parse.quote(
+            f'starts_with(@name, "{project_name}")'
+        )
+        response = requests.get(url, auth=credentials.as_tuple())
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException:
+        return ""
+
+
 def _any_timestamp_is_newer(timestamps: List[Timestamp], base: Timestamp):
     return any(ts > base for ts in timestamps)
 
@@ -89,6 +124,14 @@ def _parse_packages_list(response_text: str) -> List[str]:
 
     root = ElementTree.fromstring(response_text)
     return [package.attrib["name"] for package in root.findall("./entry")]
+
+
+def _parse_subprojects_list(response_text: str) -> List[str]:
+    if not response_text:
+        return []
+
+    root = ElementTree.fromstring(response_text)
+    return [package.attrib["name"] for package in root.findall("./project")]
 
 
 def _extract_package_timestamps(response_text) -> List[Timestamp]:

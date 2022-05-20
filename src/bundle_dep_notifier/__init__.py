2f8fc55f7a8ec806f153c4d81100e88b3e866489
Alexander Graul <agraul@suse.com>
2022-05-20 16:02:33 +0200

    Add config in TOML format

--- src/bundle_dep_notifier/__init__.py
+++ src/bundle_dep_notifier/__init__.py
@@ -2,6 +2,12 @@
 import os
 import time
 from . import obs
+import tomli
+
+
+def read_config(filename: str) -> dict:
+    with open(filename, "rb") as f:
+        return tomli.load(f)
 
 def real_test():
     # Poor man's integration test
@@ -9,7 +15,6 @@ def real_test():
     test_origin = obs.Package("home:agraul", "emacs-pgtk-nativecomp")
     print(obs.list_packages(test_origin.project, creds))
     assert (
-        obs.package_was_updated(obs.Timestamp(time.time()), test_origin, creds)
-        is False
+        obs.package_was_updated(obs.Timestamp(time.time()), test_origin, creds) is False
     )
     assert obs.package_was_updated(obs.Timestamp(0), test_origin, creds) is True
